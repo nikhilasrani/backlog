19a5d9da061a1122e905550048261327b1b5c3f0
Nikhil Asrani <asrani.nikhil@gmail.com>
2019-05-29 00:02:44 +0530

    added vectors to the signup and forgot screens

--- screens/auth/ForgotPasswordScreen.js
+++ screens/auth/ForgotPasswordScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { StyleSheet, View, Text, TextInput, Button, Alert} from "react-native";
+import { StyleSheet, View, Text, TextInput, Button, Alert, Image} from "react-native";
 import * as firebase from "firebase";
+import window from "../../constants/Layout"
 
 export default class ForgotPasswordScreen extends React.Component{
 
@@ -25,22 +26,53 @@ this.props.navigation.navigate("Login")
             }
     render(){
         return <View style={{paddingTop:50, alignItems:"center"}}>
-            <Text> Forgot Password Screen</Text>
+            <Text> Forgot Password?</Text>
+            <Image  style={styles.imageStyle} 
+            source={require("../../assets/images/ForgotPassword.png")}/>
+            <Text> Enter your email address and we will send a password reset link to you!</Text>
+            <View style={{paddingTop:15}}></View>
             <TextInput 
-            style={{width:200, height: 40, borderWidth:1}}
+            style={styles.textInput}
             value={this.state.email}
             onChangeText={(text) => {this.setState({email:text})}}
             placeholder="Email"
             keyboardType="email-address"
             autoCapitalize="none"
             autoCorrect={false}/>
-<View style={{paddingTop:15}}></View>
-            
-            <Button title="Reset Password" onPress={this.onResetPasswordPress}/>
+
             <View style={{paddingTop:15}}></View>
-            <Button title="Back to Login" onPress={this.onBackToLoginPress}/>
+            
+            <Button 
+            title="Reset Password" 
+            onPress={this.onResetPasswordPress}
+            color="#fec105"/>
+
+            <View style={{paddingTop:15}}></View>
+
+            <Button 
+            title="Back to Login" 
+            onPress={this.onBackToLoginPress}
+            color="#fec105"/>
         </View>
     }
 }
 
-const styles=  StyleSheet.create();
+const styles=  StyleSheet.create({
+    textInput: {
+        width:window.window.width,
+        height:50,
+        marginHorizontal: 25,
+        paddingHorizontal: 50,
+        borderRadius: 20,
+        borderWidth: 1
+    },
+    buttonStyle:{
+        borderRadius: 25,
+        color: "#fce105"
+    },
+    imageStyle:{
+        height:200,
+        width:200
+    }
+
+});
--- screens/auth/SignUpScreen.js
+++ screens/auth/SignUpScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { StyleSheet, View, Text, TextInput, Button, Alert} from "react-native";
+import { StyleSheet, View, Text, TextInput, Button, Alert, Image} from "react-native";
 import * as firebase from "firebase";
+import window from "../../constants/Layout"
 
 export default class SignUpScreen extends React.Component{
 
@@ -14,8 +15,7 @@ export default class SignUpScreen extends React.Component{
          };
     }
 
-    onSignUpPress = () =>
-     {
+    onSignUpPress = () =>{
         if(this.state.password!== this.state.passwordConfirm){
             Alert.alert("Passwords do not match");
             return;
@@ -32,9 +32,13 @@ export default class SignUpScreen extends React.Component{
 
     render(){
         return <View style={{paddingTop:50, alignItems:"center"}}>
-            
+<Text>Sign Up with your details below</Text>
+<View style={{paddingTop:15}}></View>
+            <Image  style={styles.imageStyle} 
+            source={require("../../assets/images/Fingerprint.png")}/>
+
             <TextInput 
-            style={{width:200, height: 40, borderWidth:1}}
+            style={styles.textInput}
             value={this.state.email}
             onChangeText={(text) => {this.setState({email:text})}}
             placeholder="Email"
@@ -42,30 +46,56 @@ export default class SignUpScreen extends React.Component{
             autoCapitalize="none"
             autoCorrect={false}/>
 
+            <View style={{paddingTop:15}}></View>
 
-            <TextInput style={{width:200, height: 40, borderWidth:1}}
+            <TextInput style={styles.textInput}
              value={this.state.password}
              onChangeText={(text) => {this.setState({password:text})}}
              placeholder="Password"
              secureTextEntry
              autoCapitalize="none"
              autoCorrect={false}/>
+
              <View style={{paddingTop:15}}></View>
             
-            <TextInput style={{width:200, height: 40, borderWidth:1}}
+            <TextInput style={styles.textInput}
              value={this.state.passwordConfirm}
              onChangeText={(text) => {this.setState({passwordConfirm:text})}}
              placeholder="Confirm Password"
              secureTextEntry
              autoCapitalize="none"
              autoCorrect={false}/> 
+
              <View style={{paddingTop:15}}></View>
 
-            <Button title="Signup" onPress={this.onSignUpPress}/>
+            <Button 
+            title="Signup" 
+            onPress={this.onSignUpPress}
+            color= "#fec105"/>
+
             <View style={{paddingTop:15}}></View>
-            <Button title="Back to Login" onPress={this.onBackToLoginPress}/>
+
+            <Button 
+            title="Back to Login" 
+            onPress={this.onBackToLoginPress}
+            color= "#fec105"/>
         </View>
     }
 }
 
-const styles=  StyleSheet.create();
+const styles=  StyleSheet.create(
+    {
+        textInput: {
+            width:window.window.width,
+            height:50,
+            marginHorizontal: 25,
+            paddingHorizontal: 50,
+            borderRadius: 20,
+            borderWidth: 1
+        },
+        imageStyle:{
+            height:200,
+            width:200
+        }
+    }
+);
