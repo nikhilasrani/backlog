11eab00a96d970c3c570d7ce5c8980e6c8e140a0
Nikhil Asrani <asrani.nikhil@gmail.com>
2019-06-13 10:12:44 +0530

    added custom reddit and twitter cards

--- screens/TestScreen.js
+++ screens/TestScreen.js
@@ -7,6 +7,8 @@ import {
 } from 'react-native';
 import window from "../constants/Layout"
 import * as firebase from "firebase"
+import {Card} from "react-native-elements";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default class TestScreen extends React.Component {
   static navigationOptions = {
@@ -32,8 +34,7 @@ export default class TestScreen extends React.Component {
   }
 
   renderFooter = () => {
-    if(!this.state.loading) return null;
-    
+    if(!this.state.loading) return <View style={{paddingTop:35}}></View>;
     return(
       <View style={{paddingVertical:30,}}>
         <ActivityIndicator animating size="large"/>
@@ -50,20 +51,13 @@ export default class TestScreen extends React.Component {
 });
 
   }
-  
-//   renderListItem = ({item}) => {
-// <View>
-//   <Text>{item.link}</Text>
-// </View>
-//   }
-
 _fetchUserLinks = () => {
   var user = firebase.auth().currentUser;
     var that = this;
     const {page, seed} = that.state;
     that.setState({loading:true});
     if (user != null) {
-      firebase.database().ref(`users/${user.uid}/savedLinks/`).once('value', function(snapshot){
+      return firebase.database().ref(`users/${user.uid}/savedLinks/`).once('value', function(snapshot){
         console.log(snapshot.val());
         //^ Firebase response as a JSON Object
         const linksToArray =Object.entries(snapshot.val()).map(item => ({...item[1], key: item[0]}));
@@ -72,15 +66,53 @@ _fetchUserLinks = () => {
         // ^ JSON Object converted to an Array of Objects with the unique value as a key
         that.setState({links:linksToArray,loading:false, refreshing:false});
       })}
+
 }
 _renderItem = ({item}) => {
+  var url=item.link.url.toString();
+    
+  //Checking if the link is Twitter 
+  if(url.substring(0,15)==='https://twitter'){
+    return <Card containerStyle={{borderRadius:8}}>
+      <View style={{flexDirection:"row", justifyContent:"space-between"}}>
+        <View style={{flexDirection:"row"}}>
+          <Image style={{height:40,width:40, borderRadius:8}} source={{uri:item.link.images[0]}}/>
+          <Text style={{paddingHorizontal:12, fontWeight:"bold", fontSize:16}}>{item.link.title.substring(0,item.link.title.length-10)}</Text>
+        </View>
+      <MaterialCommunityIcons name="twitter" size={20}	color="#1da1f2"/>
+      </View>
+      <Text style={{paddingTop:10, color:"#000"}}>{item.link.description.substring(1,item.link.description.length-1)}</Text>
+      </Card>
+  }
+
+  //Checking if the link is Reddit
+  if(url.substring(0,18)==='https://www.reddit'){
+    var hyphen= item.link.title.indexOf("-");
+    var subreddit = item.link.title.substring(0,hyphen-1);
+    var redditTitle = item.link.title.substring(hyphen+2,item.link.title.length);
+    return <Card containerStyle={{borderRadius:8}}>
+      <View style={{flexDirection:"row", justifyContent:"space-between"}}>
+        <View style={{}}>
+          <Text style={{paddingHorizontal:12, fontWeight:"bold", fontSize:16}}>{subreddit}</Text>
+        </View>
+      <MaterialCommunityIcons name="reddit" size={20}	color="#FF5700"/>
+      </View>
+      <Text style={{paddingVertical:15, color:"#000", fontSize:20,textAlign:"center"}}>{redditTitle}</Text>
+      <Image style={{height:200,paddingTop:10, borderRadius:8, resizeMode:"contain"}} source={{uri:item.link.images[0]}}/>
+      </Card>
+  }
+
+
+
   switch(item.link.mediaType){
     case 'article':
       //Twitter tweets shared from the UI
-     return  <View style={styles.cardStyle}><Text>{item.link.description}</Text></View>
+     return  <Card title={item.link.title} image={{uri:item.link.images[0]}}><Text>{item.link.description}</Text></Card>
     case 'image':
+        return  <Card title={item.link.title} image={{uri:item.link.images[0]}}><Text>{item.link.description}</Text></Card>
     case 'video':
     case 'website':
+        return  <Card title={item.link.title}><Text>{item.link.description}</Text></Card>
     case 'application':
     case 'audio':
     
@@ -89,6 +121,7 @@ _renderItem = ({item}) => {
   }
 
 }
+
 
   render() {
     return (
